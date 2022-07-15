4e59d2e8eb5afde00e81806f6a7fefb0bf22343c
Ahmed <ahmed.abdelbakey.18@gmail.com>
2022-07-16 00:38:20 +0300

    [ADD] Add purchase_product model

--- models/purchase_product.py
+++ models/purchase_product.py
@@ -1,5 +1,21 @@
+from email.policy import default
+
 import required
 
 from odoo import models, api, fields
 
-product_id = fields.Many2one('product.product', required=True)
+
+class purchaseProduct(models.Model):
+    _name = 'purchase.product'
+
+    product_id = fields.Many2one('product.product', required=True)
+    description = fields.Char(related='product_id.name', string="Description")
+    quality = fields.Integer(default=1)
+    cost = fields.Float(related='product_id.standard_price', string='Cost', readonly=True)
+    total = fields.Float(string='Total', compute='get_cost', readonly=True)
+    request_id = fields.Many2one('purchase.request')
+
+    @api.depends('quality', 'cost')
+    def get_cost(self):
+        for record in self:
+            record.total = record.quality * record.cost
