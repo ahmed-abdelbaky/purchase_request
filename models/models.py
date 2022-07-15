d4758a4d075a37f218ececa6bd5a11ef04786dc7
Ahmed <ahmed.abdelbakey.18@gmail.com>
2022-07-16 00:37:12 +0300

    [ADD] Add new purchase_request model

--- models/models.py
+++ models/models.py
@@ -1,18 +1,27 @@
-# -*- coding: utf-8 -*-
+from email.policy import default
 
-# from odoo import models, fields, api
+import required
+
+from odoo import models, fields, api
 
 
-# class purchase_request(models.Model):
-#     _name = 'purchase_request.purchase_request'
-#     _description = 'purchase_request.purchase_request'
+class purchase_request(models.Model):
+    _name = 'purchase.request'
+    _description = 'purchase_request.purchase_request'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    name = fields.Char(required=True)
+    request_id = fields.Many2one("res.users", string="request by", required=True)
+    start_date = fields.Date(default=fields.Date.today)
+    end_date = fields.Date()
+    reject_reason = fields.Text(Readonly=True, string="Reject Reason")
+    product_ids = fields.One2many('purchase.product', 'request_id', string="Purchase Request Line")
+    total_price = fields.Float(string="Total Price", compute="get_total", store=True)
+
+    @api.depends("product_ids.cost")
+    def get_total(self):
+
+        for record in self:
+            comm = 0.0
+            for line in record.product_ids:
+                comm += line.cost
+                record.total_price = comm
