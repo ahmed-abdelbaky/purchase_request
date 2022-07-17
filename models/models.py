71246274c640ae6729bb2a8452372fb2e914dcee
Ahmed <ahmed.abdelbakey.18@gmail.com>
2022-07-18 00:30:01 +0300

    [ADD] add states selecton field

--- models/models.py
+++ models/models.py
@@ -16,6 +16,30 @@ class purchase_request(models.Model):
     reject_reason = fields.Text(Readonly=True, string="Reject Reason")
     product_ids = fields.One2many('purchase.product', 'request_id', string="Purchase Request Line")
     total_price = fields.Float(string="Total Price", compute="get_total", store=True)
+    state = fields.Selection(
+        [
+            ('draft', 'Reset to draft'),
+            ('to_be_approve', 'Submit for Approval'),
+            ('approve', 'Approve'),
+            ('reject', 'Reject'),
+            ('cancel', 'Cancel')
+        ], string="status", default='draft'
+    )
+
+    def set_draft(self):
+        self.state = 'draft'
+
+    def to_be_approve(self):
+        self.state = 'to_be_approve'
+
+    def set_cancel(self):
+        self.state = 'to_be_approve'
+
+    def set_approve(self):
+        self.state = 'approve'
+
+    def set_reject(self):
+        self.state = 'reject'
 
     @api.depends("product_ids.cost")
     def get_total(self):
