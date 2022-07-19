62b0eca4fe804480047c680ac3dad1d235056e28
Ahmed <ahmed.abdelbakey.18@gmail.com>
2022-07-19 23:21:13 +0300

    [FIX] fix bug return data from wizard

--- wizard/wizard.py
+++ wizard/wizard.py
@@ -1,17 +1,18 @@
+from email.policy import default
+
 from odoo import fields, models, api
 
 
 class wizard(models.TransientModel):
     _name = 'purchase.wizard'
 
+    def _default_sessions(self):
+        return self.env['purchase.request'].browse(self._context.get('active_ids'))
+
+    reason_ids = fields.Many2one('purchase.request', default=_default_sessions)
     reason = fields.Text(string='Reason')
 
     def confirm(self):
-        self.env['purchase.request'].create(
-            {
-                "reject_reason": self.reason,
-                'state': 'reject'
-            }
-
-        )
+        self.reason_ids.reject_reason = self.reason,
+        self.reason_ids.state = 'reject'
 
